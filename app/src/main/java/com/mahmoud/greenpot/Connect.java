16ad86c05992890a1c5980df80fae2e2bba3cf2f
AhmedBadee <ahmedbadee17@gmail.com>
2018-02-16 08:45:13 +0200

    the app can connect to a hidden wifi network with known ssid and pre shared key

--- app/src/main/java/com/mahmoud/greenpot/Connect.java
+++ app/src/main/java/com/mahmoud/greenpot/Connect.java
@@ -1,13 +1,64 @@
 package com.mahmoud.greenpot;
 
+import android.content.Context;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Connect extends AppCompatActivity {
+
+    @BindView(R.id.et_ssid)
+    EditText etSSID;
+
+    @BindView(R.id.et_password)
+    EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_connect)
+    public void onConnect() {
+        Log.e("CONNECT", "Starting Function");
+        WifiConfiguration wifiConfiguration = new WifiConfiguration();
+        wifiConfiguration.SSID = "\"" + etSSID.getText().toString() + "\"";
+        wifiConfiguration.preSharedKey = "\"" + etPassword.getText().toString() + "\"";
+
+        wifiConfiguration.hiddenSSID = true;
+        wifiConfiguration.status = WifiConfiguration.Status.ENABLED;
+        wifiConfiguration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+        wifiConfiguration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+        wifiConfiguration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+        wifiConfiguration.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+        wifiConfiguration.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+        wifiConfiguration.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+        wifiConfiguration.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+
+        WifiManager wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (wifiManager != null) {
+            int networkId = wifiManager.addNetwork(wifiConfiguration);
+            wifiManager.disconnect();
+            wifiManager.enableNetwork(networkId, true);
+            wifiManager.reconnect();
+            Log.e("CONNECT", "Connecting");
+        }
     }
 }
