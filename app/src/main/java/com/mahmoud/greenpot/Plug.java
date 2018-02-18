9bf6eeb93c5cd66e113f028ec08ff08b3f0ec7b3
AhmedBadee <ahmedbadee17@gmail.com>
2018-02-18 16:43:53 +0200

    added MQTT connection options and publish method, added a checked event for the first switch

--- app/src/main/java/com/mahmoud/greenpot/Plug.java
+++ app/src/main/java/com/mahmoud/greenpot/Plug.java
@@ -2,12 +2,83 @@ package com.mahmoud.greenpot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Plug extends AppCompatActivity {
+
+    private static String MQTTHOST = "tcp://m23.cloudmqtt.com:13207";
+    private static String USERNAME = "ruafzbed";
+    private static String PASSWORD = "QK7Hde2Drz2r";
+
+    private MqttAndroidClient client;
+
+    @BindView(R.id.switch_first)
+    Switch switchFirst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plug);
+        ButterKnife.bind(this);
+
+        String clientId = MqttClient.generateClientId();
+
+        client = new MqttAndroidClient(this.getApplicationContext(), MQTTHOST, clientId);
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setUserName(USERNAME);
+        mqttConnectOptions.setPassword(PASSWORD.toCharArray());
+
+        try {
+            IMqttToken iMqttToken = client.connect(mqttConnectOptions);
+            iMqttToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.e("CONNECTION", "Connected");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.e("CONNECTION", "Failed");
+                }
+            });
+
+        } catch (MqttException exception) {
+            exception.printStackTrace();
+        }
+
+        switchFirst.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    publish("ON");
+                } else {
+                    publish("OFF");
+                }
+            }
+        });
+    }
+
+    private void publish(String message) {
+        String topic = "inTopic";
+
+        try {
+            client.publish(topic, message.getBytes(), 0, false);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
